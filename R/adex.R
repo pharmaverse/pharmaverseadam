4a7d86b9ad131bc5378391b0379dd022fad29ecb
dgrassellyb <dgrassellyb@users.noreply.github.com>
2023-09-08 15:04:27 +0000

    templates data updates from pharmaverse/admiral

---
+++ R/adex.R
@@ -0,0 +1,104 @@
+#' Dataset adex
+#'
+#' adex dataset
+#'
+#' @name adex
+#' @docType data
+#' @format A data frame with 88 columns:
+#'   \describe{
+#'     \item{ STUDYID }{STUDYID}
+#'     \item{ DOMAIN }{DOMAIN}
+#'     \item{ USUBJID }{USUBJID}
+#'     \item{ EXSEQ }{EXSEQ}
+#'     \item{ EXTRT }{EXTRT}
+#'     \item{ EXDOSE }{EXDOSE}
+#'     \item{ EXDOSU }{EXDOSU}
+#'     \item{ EXDOSFRM }{EXDOSFRM}
+#'     \item{ EXDOSFRQ }{EXDOSFRQ}
+#'     \item{ EXROUTE }{EXROUTE}
+#'     \item{ VISITNUM }{VISITNUM}
+#'     \item{ VISIT }{VISIT}
+#'     \item{ VISITDY }{VISITDY}
+#'     \item{ EXSTDTC }{EXSTDTC}
+#'     \item{ EXENDTC }{EXENDTC}
+#'     \item{ EXSTDY }{EXSTDY}
+#'     \item{ EXENDY }{EXENDY}
+#'     \item{ EXADJ }{EXADJ}
+#'     \item{ EXPLDOS }{EXPLDOS}
+#'     \item{ TRTSDT }{TRTSDT}
+#'     \item{ TRTSDTM }{TRTSDTM}
+#'     \item{ TRTEDTM }{TRTEDTM}
+#'     \item{ ASTDTM }{ASTDTM}
+#'     \item{ ASTDTF }{ASTDTF}
+#'     \item{ ASTTMF }{ASTTMF}
+#'     \item{ AENDTM }{AENDTM}
+#'     \item{ AENDTF }{AENDTF}
+#'     \item{ AENTMF }{AENTMF}
+#'     \item{ ASTDY }{ASTDY}
+#'     \item{ AENDY }{AENDY}
+#'     \item{ EXDURD }{EXDURD}
+#'     \item{ ASTDT }{ASTDT}
+#'     \item{ AENDT }{AENDT}
+#'     \item{ DOSEO }{DOSEO}
+#'     \item{ PDOSEO }{PDOSEO}
+#'     \item{ PARAMCD }{PARAMCD}
+#'     \item{ AVAL }{AVAL}
+#'     \item{ AVALC }{AVALC}
+#'     \item{ PARCAT1 }{PARCAT1}
+#'     \item{ PARAM }{PARAM}
+#'     \item{ PARAMN }{PARAMN}
+#'     \item{ AVALCAT1 }{AVALCAT1}
+#'     \item{ ASEQ }{ASEQ}
+#'     \item{ SUBJID }{SUBJID}
+#'     \item{ RFSTDTC }{RFSTDTC}
+#'     \item{ RFENDTC }{RFENDTC}
+#'     \item{ RFXSTDTC }{RFXSTDTC}
+#'     \item{ RFXENDTC }{RFXENDTC}
+#'     \item{ RFICDTC }{RFICDTC}
+#'     \item{ RFPENDTC }{RFPENDTC}
+#'     \item{ DTHDTC }{DTHDTC}
+#'     \item{ DTHFL }{DTHFL}
+#'     \item{ SITEID }{SITEID}
+#'     \item{ AGE }{AGE}
+#'     \item{ AGEU }{AGEU}
+#'     \item{ SEX }{SEX}
+#'     \item{ RACE }{RACE}
+#'     \item{ ETHNIC }{ETHNIC}
+#'     \item{ ARMCD }{ARMCD}
+#'     \item{ ARM }{ARM}
+#'     \item{ ACTARMCD }{ACTARMCD}
+#'     \item{ ACTARM }{ACTARM}
+#'     \item{ COUNTRY }{COUNTRY}
+#'     \item{ DMDTC }{DMDTC}
+#'     \item{ DMDY }{DMDY}
+#'     \item{ TRT01P }{TRT01P}
+#'     \item{ TRT01A }{TRT01A}
+#'     \item{ TRTSTMF }{TRTSTMF}
+#'     \item{ TRTETMF }{TRTETMF}
+#'     \item{ TRTEDT }{TRTEDT}
+#'     \item{ TRTDURD }{TRTDURD}
+#'     \item{ SCRFDT }{SCRFDT}
+#'     \item{ EOSDT }{EOSDT}
+#'     \item{ EOSSTT }{EOSSTT}
+#'     \item{ FRVDT }{FRVDT}
+#'     \item{ RANDDT }{RANDDT}
+#'     \item{ DTHDT }{DTHDT}
+#'     \item{ DTHADY }{DTHADY}
+#'     \item{ LDDTHELD }{LDDTHELD}
+#'     \item{ LSTALVDT }{LSTALVDT}
+#'     \item{ AGEGR1 }{AGEGR1}
+#'     \item{ SAFFL }{SAFFL}
+#'     \item{ RACEGR1 }{RACEGR1}
+#'     \item{ REGION1 }{REGION1}
+#'     \item{ LDDTHGR1 }{LDDTHGR1}
+#'     \item{ DTH30FL }{DTH30FL}
+#'     \item{ DTHA30FL }{DTHA30FL}
+#'     \item{ DTHB30FL }{DTHB30FL}
+#'   }
+#'
+#' @source Generated from pharmaverse/admiral.
+#' @references None
+#'
+#' @examples
+#' load(adex)
+"dataset_name"
