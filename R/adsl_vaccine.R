2698f251b225a5cf2b790618ffa37b3c186aae75
dgrassellyb <dgrassellyb@users.noreply.github.com>
2023-09-11 12:16:57 +0000

    templates data updates from pharmaverse/admiralvaccine

---
+++ R/adsl_vaccine.R
@@ -0,0 +1,62 @@
+#' Dataset adsl_vaccine
+#'
+#' adsl_vaccine dataset
+#'
+#' @name adsl_vaccine
+#' @docType data
+#' @format A data frame with 46 columns:
+#'   \describe{
+#'     \item{ STUDYID }{STUDYID}
+#'     \item{ USUBJID }{USUBJID}
+#'     \item{ SUBJID }{SUBJID}
+#'     \item{ RFSTDTC }{RFSTDTC}
+#'     \item{ RFENDTC }{RFENDTC}
+#'     \item{ RFXSTDTC }{RFXSTDTC}
+#'     \item{ RFXENDTC }{RFXENDTC}
+#'     \item{ RFICDTC }{RFICDTC}
+#'     \item{ RFPENDTC }{RFPENDTC}
+#'     \item{ DTHDTC }{DTHDTC}
+#'     \item{ DTHFL }{DTHFL}
+#'     \item{ SITEID }{SITEID}
+#'     \item{ INVID }{INVID}
+#'     \item{ INVNAM }{INVNAM}
+#'     \item{ BRTHDTC }{BRTHDTC}
+#'     \item{ AGE }{AGE}
+#'     \item{ AGEU }{AGEU}
+#'     \item{ SEX }{SEX}
+#'     \item{ RACE }{RACE}
+#'     \item{ ETHNIC }{ETHNIC}
+#'     \item{ ARMCD }{ARMCD}
+#'     \item{ ARM }{ARM}
+#'     \item{ ACTARMCD }{ACTARMCD}
+#'     \item{ ACTARM }{ACTARM}
+#'     \item{ COUNTRY }{COUNTRY}
+#'     \item{ DMDTC }{DMDTC}
+#'     \item{ DMDY }{DMDY}
+#'     \item{ TRT01P }{TRT01P}
+#'     \item{ TRT02P }{TRT02P}
+#'     \item{ TRT01A }{TRT01A}
+#'     \item{ TRT02A }{TRT02A}
+#'     \item{ TRTSDTM }{TRTSDTM}
+#'     \item{ TRTEDTM }{TRTEDTM}
+#'     \item{ TRTSDT }{TRTSDT}
+#'     \item{ TRTEDT }{TRTEDT}
+#'     \item{ SAFFL }{SAFFL}
+#'     \item{ PPROTFL }{PPROTFL}
+#'     \item{ RACEGR1 }{RACEGR1}
+#'     \item{ AGEGR1 }{AGEGR1}
+#'     \item{ REGION1 }{REGION1}
+#'     \item{ VAX01DT }{VAX01DT}
+#'     \item{ VAX02DT }{VAX02DT}
+#'     \item{ AP01SDT }{AP01SDT}
+#'     \item{ AP01EDT }{AP01EDT}
+#'     \item{ AP02SDT }{AP02SDT}
+#'     \item{ AP02EDT }{AP02EDT}
+#'   }
+#'
+#' @source Generated from pharmaverse/admiralvaccine.
+#' @references None
+#'
+#' @examples
+#' data("adsl_vaccine")
+"adsl_vaccine"
