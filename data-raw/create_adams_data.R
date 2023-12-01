dbe87d83ffab37e65841f11c8707816540face78
Daphné Grasselly <56442075+dgrassellyb@users.noreply.github.com>
2023-12-01 15:32:20 +0100

    fix checks

--- data-raw/create_adams_data.R
+++ data-raw/create_adams_data.R
@@ -2,7 +2,7 @@
 
 update_pkg <- FALSE
 ignore_templates <- list(
-    "admiralonco" = c("ad_adrs_basic.R")
+  "admiralonco" = c("ad_adrs_basic.R")
 )
 
 save_rda <- function(data, file_path, new_name) {
@@ -14,15 +14,15 @@ save_rda <- function(data, file_path, new_name) {
   }
 }
 
-load_rda <- function(fileName){
-    load(fileName)
-    get(ls()[ls() != "fileName"])
+load_rda <- function(fileName) {
+  load(fileName)
+  get(ls()[ls() != "fileName"])
 }
 
 write_doc <- function(data, dataset_name) {
-    # create documentation for the current dataset
-    # TODO: use metatools/metacore for doc  ?
-    doc_string <- paste(
+  # create documentation for the current dataset
+  # TODO: use metatools/metacore for doc  ?
+  doc_string <- paste(
     sprintf("#' Dataset %s", dataset_name),
     "#'",
     sprintf("#' %s dataset", dataset_name),
@@ -32,127 +32,136 @@ write_doc <- function(data, dataset_name) {
     sprintf("#' @format A data frame with %s columns:", ncol(data)),
     "#'   \\describe{",
     paste(sapply(names(data), function(col_name) {
-    paste(sprintf("#'     \\item{ %s }{%s}", col_name, col_name))
-    }, USE.NAMES = FALSE), collapse="\n"),
+      paste(sprintf("#'     \\item{ %s }{%s}", col_name, col_name))
+    }, USE.NAMES = FALSE), collapse = "\n"),
     "#'   }",
     "#'", sprintf("#' @source Generated from create_adams_data.R script."),
     "#' @references None",
     "#'", sprintf("#' @examples\n#' data(\"%s\")", dataset_name),
     sep = "\n",
     sprintf("\"%s\"", dataset_name)
-    )
-    writeLines(doc_string, con = file.path("R", paste0(dataset_name, ".R")))
+  )
+  writeLines(doc_string, con = file.path("R", paste0(dataset_name, ".R")))
 }
 
 write_labels <- function(data, dataset_name, suffix) {
-    print(dataset_name)
-    print(suffix)
-    if (suffix != "") {
-        dataset_name <- gsub(suffix, suffixes_dict[suffix], dataset_name)
+  print(dataset_name)
+  print(suffix)
+  if (suffix != "") {
+    dataset_name <- gsub(suffix, suffixes_dict[suffix], dataset_name)
+  }
+  dataset_name <- toupper(dataset_name)
+  tryCatch(
+    {
+      spec <- metacore::select_dataset(mc, dataset_name)
+      data <- metatools::set_variable_labels(data, spec)
+    },
+    error = function(e) {
+      warning(sprintf(
+        "Error retrieving dataset %s specs - Please check adams-specs.xlsx file",
+        dataset_name
+      ))
     }
-    dataset_name <- toupper(dataset_name)
-    tryCatch({
-        spec <- metacore::select_dataset(mc, dataset_name)
-        data <- metatools::set_variable_labels(data, spec)
-    }, error = function(e) {
-        warning(sprintf("Error retrieving dataset %s specs - Please check adams-specs.xlsx file",
-        dataset_name))
-    }
-    )
-    return(data)
+  )
+  return(data)
 }
 
 run_template <- function(tp) {
-    if (!tp %in% ignore_templates_pkg) {
-        print(sprintf("Running template %s", tp))
-        # run template
-        cmd <- c("Rscript", "--vanilla", file.path(templates_path, tp))
-        system_result <- system2(cmd, stdout = TRUE, stderr = TRUE)
-        exit_code <- attr(system_result, "status")
+  if (!tp %in% ignore_templates_pkg) {
+    print(sprintf("Running template %s", tp))
+    # run template
+    cmd <- c("Rscript", "--vanilla", file.path(templates_path, tp))
+    system_result <- system2(cmd, stdout = TRUE, stderr = TRUE)
+    exit_code <- attr(system_result, "status")
 
-        if (is.null(exit_code)) {
-            dataset_dir <- tools::R_user_dir(sprintf("%s_templates_data", pkg), which = "cache")
-            rda_file <- gsub(".R", ".rda", basename(tp))
-            rda_file <- gsub("ad_", "", rda_file)
-            data <- load_rda(file.path(dataset_dir, rda_file))
+    if (is.null(exit_code)) {
+      dataset_dir <- tools::R_user_dir(sprintf("%s_templates_data", pkg), which = "cache")
+      rda_file <- gsub(".R", ".rda", basename(tp))
+      rda_file <- gsub("ad_", "", rda_file)
+      data <- load_rda(file.path(dataset_dir, rda_file))
 
-            print(sprintf("Processing %s file - move it to
+      print(sprintf("Processing %s file - move it to
              pharmaverse and generate the doc", rda_file))
-            suffix <- ""
-            if (pkg != "admiral") {
-                suffix <- sprintf("_%s", gsub("admiral", "", pkg))
-            }
-            # add suffix in case of != admiral
-            filename <- gsub("\\.rda$", sprintf("%s.rda", suffix), rda_file)
-            output_adam_path <- file.path("data", filename)
-            dataset_name <- gsub("\\.rda$", "", filename)
+      suffix <- ""
+      if (pkg != "admiral") {
+        suffix <- sprintf("_%s", gsub("admiral", "", pkg))
+      }
+      # add suffix in case of != admiral
+      filename <- gsub("\\.rda$", sprintf("%s.rda", suffix), rda_file)
+      output_adam_path <- file.path("data", filename)
+      dataset_name <- gsub("\\.rda$", "", filename)
 
-            # write labels
-            data <- write_labels(data, dataset_name, suffix)
+      # write labels
+      data <- write_labels(data, dataset_name, suffix)
 
-            # save it to pharmaverseadam data package
-            save_rda(data, file_path = output_adam_path, new_name = dataset_name)
+      # save it to pharmaverseadam data package
+      save_rda(data, file_path = output_adam_path, new_name = dataset_name)
 
-            # write doc
-            write_doc(data, dataset_name = dataset_name)
-        }
-
-        # return output cmd from templates
-        return(list(pkg = pkg, template = tp, exit_code = exit_code,
-        output = paste(system_result, collapse = "\n")))
+      # write doc
+      write_doc(data, dataset_name = dataset_name)
     }
+
+    # return output cmd from templates
+    return(list(
+      pkg = pkg, template = tp, exit_code = exit_code,
+      output = paste(system_result, collapse = "\n")
+    ))
+  }
 }
 
 
-if (update_pkg){
-    github_pat <- Sys.getenv("GITHUB_TOKEN") # in case of run through github workflows
-    if (github_pat == ""){
-        token_file <- "data-raw/github_token.R"
-        if (file.exists(token_file)){
-            source(token_file)
-        } else {
-            "WARNING - you might have issues installing pharmaverse deps -
+if (update_pkg) {
+  github_pat <- Sys.getenv("GITHUB_TOKEN") # in case of run through github workflows
+  if (github_pat == "") {
+    token_file <- "data-raw/github_token.R"
+    if (file.exists(token_file)) {
+      source(token_file)
+    } else {
+      "WARNING - you might have issues installing pharmaverse deps -
             Please follow instructions in the readme.md"
-        }
     }
-    # install pharmaversesdtm dep: TODO: see if we install from github or latest release?
-    install.packages("pharmaverse/pharmaversesdtm")
+  }
+  # install pharmaversesdtm dep: TODO: see if we install from github or latest release?
+  install.packages("pharmaverse/pharmaversesdtm")
 }
 
 # dict to match admiral xlsx specs suffixes
 suffixes_dict <- list("_ophta" = "_P", "_onco" = "_O", "_vaccine" = "_V")
-mc <- metacore::spec_to_metacore("inst/extdata/adams-specs.xlsx", where_sep_sheet = FALSE,
-        quiet = TRUE)
+mc <- metacore::spec_to_metacore("inst/extdata/adams-specs.xlsx",
+  where_sep_sheet = FALSE,
+  quiet = TRUE
+)
 
 packages_list <- c("admiral", "admiralonco", "admiralophtha", "admiralvaccine")
 all_results <- c()
-for (pkg in packages_list){
-    ignore_templates_pkg <- ignore_templates[pkg]
-    # get latest version of the package (current templates from main branch)
-    if (update_pkg) {
-        # TODO: replace by main once done
-        remotes::install_github(sprintf("pharmaverse/%s", pkg),
-        ref = "workflow_dispatch_templates", auth_token = github_pat,
-        upgrade = "always", force = TRUE)
-    }
-    templates_path <- file.path(system.file(package = pkg), "templates")
-    templates <- list.files(templates_path)
+for (pkg in packages_list) {
+  ignore_templates_pkg <- ignore_templates[pkg]
+  # get latest version of the package (current templates from main branch)
+  if (update_pkg) {
+    # TODO: replace by main once done
+    remotes::install_github(sprintf("pharmaverse/%s", pkg),
+      ref = "workflow_dispatch_templates", auth_token = github_pat,
+      upgrade = "always", force = TRUE
+    )
+  }
+  templates_path <- file.path(system.file(package = pkg), "templates")
+  templates <- list.files(templates_path)
 
-    print(sprintf("Processing templates from %s package", pkg))
+  print(sprintf("Processing templates from %s package", pkg))
 
-    # run templates in parallel
-    results <- parallel::mclapply(templates, run_template, mc.cores = length(templates))
-    all_results <- c(all_results, results)
+  # run templates in parallel
+  results <- parallel::mclapply(templates, run_template, mc.cores = length(templates))
+  all_results <- c(all_results, results)
 }
 
 
 for (res in all_results) {
-    if (!is.null(res$exit_code)) {
-        print(sprintf("template %s failed - package %s", res$template, res$pkg))
-        print("error:")
-        cat(sprintf("%s\n\n", res$output))
-    }
+  if (!is.null(res$exit_code)) {
+    print(sprintf("template %s failed - package %s", res$template, res$pkg))
+    print("error:")
+    cat(sprintf("%s\n\n", res$output))
+  }
 }
 
 # Generate the documentation
-roxygen2::roxygenize('.', roclets = c('rd', 'collate', 'namespace'))
+roxygen2::roxygenize(".", roclets = c("rd", "collate", "namespace"))
