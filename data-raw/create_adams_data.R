c3bb3b8ebf5b37d4466ddc434ba5c91e5051a6d6
Lina2689 <lina.patil@cytel.com>
2025-09-17 14:31:02 +0530

    #128: Updates to order and group the varaibales as per standards

--- data-raw/create_adams_data.R
+++ data-raw/create_adams_data.R
@@ -28,7 +28,8 @@ library(stringr)
 update_pkg <- TRUE
 ignore_templates <- list(
   "admiralonco" = c("ad_adrs_basic.R"),
-  "admiralmetabolic" = c("ad_adsl_metabolic.R")
+  "admiralmetabolic" = c("ad_adsl_metabolic.R"),
+  "admiralvaccine" = c("ad_adsl.R")
 )
 
 save_rda <- function(data, file_path, new_name) {
@@ -123,6 +124,28 @@ write_labels <- function(data, dataset_name, suffix) {
   return(data)
 }
 
+order_data <- function(data, dataset_name, suffix){
+  print(dataset_name)
+  print(suffix)
+  if (suffix != "") {
+    dataset_name <- gsub(suffix, suffixes_dict[suffix], dataset_name)
+  }
+  dataset_name <- toupper(dataset_name)
+  tryCatch(
+    {
+      spec <- metacore::select_dataset(mc, dataset_name)
+      data <- metatools::order_cols(data, spec)
+    },
+    error = function(e) {
+      warning(sprintf(
+        "Error retrieving dataset %s specs - Please check adams-specs.xlsx file",
+        dataset_name
+      ))
+    }
+  )
+  return(data)
+}
+
 run_template <- function(tp) {
   if (!tp %in% ignore_templates_pkg) {
     print(sprintf("Running template %s", tp))
@@ -186,6 +209,9 @@ run_template <- function(tp) {
 
       # write labels
       data <- write_labels(data, dataset_name, suffix)
+
+      # Order columns as per specs
+      data <- order_data(data, dataset_name, suffix)
 
       # save it to pharmaverseadam data package
       save_rda(data, file_path = output_adam_path, new_name = dataset_name)
