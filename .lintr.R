0585e3cdeb4afce03bbff7626cb94f49151dccbb
Fanny-Gautier <fanny.gautier@cytel.com>
2025-10-24 17:11:33 +0200

    #133 Update .lintr.R

--- .lintr.R
+++ .lintr.R
@@ -4,7 +4,7 @@ source(system.file("lintr/linters.R", package = "admiraldev"))
 
 linters <- admiral_linters()
 
-exclusions: list(
+exclusions <- list(
     "data-raw/create_adams_data.R" = Inf,
     "R/data.R" = Inf,
     "inst" = list(undesirable_function_linter = Inf),
