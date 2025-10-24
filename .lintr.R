d6ac981e3416143537ca09f2838890dfa25e3d3d
Fanny-Gautier <fanny.gautier@cytel.com>
2025-10-24 16:34:17 +0200

    #133 Rename the .lintr config file to .lintr.R, source the admiraldev file

---
+++ .lintr.R
@@ -0,0 +1,12 @@
+library(lintr)
+
+source(system.file("lintr/linters.R", package = "admiraldev"))
+
+linters <- admiral_linters()
+
+exclusions: list(
+    "data-raw/create_adams_data.R" = Inf,
+    "R/data.R" = Inf,
+    "inst" = list(undesirable_function_linter = Inf),
+    "vignettes" = list(undesirable_function_linter = Inf)
+  )
