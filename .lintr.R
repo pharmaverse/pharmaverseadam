b552e6735c9ee8a8d2aae57aaaaca767d262f41e
Fanny-Gautier <fanny.gautier@cytel.com>
2025-10-31 11:07:51 +0100

    #133 styler

--- .lintr.R
+++ .lintr.R
@@ -5,8 +5,8 @@ source(system.file("lintr/linters.R", package = "admiraldev"))
 linters <- admiral_linters()
 
 exclusions <- list(
-    "data-raw/create_adams_data.R" = Inf,
-    "R/data.R" = Inf,
-    "inst" = list(undesirable_function_linter = Inf),
-    "vignettes" = list(undesirable_function_linter = Inf)
-  )
+  "data-raw/create_adams_data.R" = Inf,
+  "R/data.R" = Inf,
+  "inst" = list(undesirable_function_linter = Inf),
+  "vignettes" = list(undesirable_function_linter = Inf)
+)
